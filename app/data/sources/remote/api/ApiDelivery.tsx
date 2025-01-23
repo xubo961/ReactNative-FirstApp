f308f3c9fbf353a1f1051f7655540ab71483e138
xubo961 <xubop961@gmail.com>
2025-01-23 14:34:06 +0100

    fixed error axios conection network

--- app/data/sources/remote/api/ApiDelivery.tsx
+++ app/data/sources/remote/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiDelivery = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://192.168.73.152:8080/api",
     headers: {
         "Content-Type": "application/json",
     }
